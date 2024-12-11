1d85daaf0dc33956d1ae291e45eb6c7da1d2cc95
Vasyl Gello <vasek.gello@gmail.com>
2024-12-11 09:12:56 +0200

    Add nonfree drivers
    Signed-off-by: Vasyl Gello <vasek.gello@gmail.com>

--- supportFiles/installChroot.sh
+++ supportFiles/installChroot.sh
@@ -20,7 +20,30 @@ update-locale LANG=en_US.UTF-8
 
 echo Install packages
 apt-get install -y --no-install-recommends linux-image-amd64 live-boot systemd-sysv
-apt-get install -y bash-completion cifs-utils curl dbus dosfstools firmware-linux-free gddrescue gdisk iputils-ping isc-dhcp-client iw iwd less nfs-common ntfs-3g openssh-client open-vm-tools procps vim wimtools wget
+apt-get install -y \
+  bash-completion \
+  cifs-utils \
+  curl \
+  dbus \
+  dosfstools \
+  firmware-linux-free \
+  firmware-linux-nonfree \
+  gddrescue \
+  gdisk \
+  iputils-ping \
+  isc-dhcp-client \
+  iw \
+  iwd \
+  less \
+  nfs-common \
+  ntfs-3g \
+  openssh-client \
+  open-vm-tools \
+  procps \
+  rfkill \
+  vim \
+  wimtools \
+  wget
 
 echo Clean apt post-install
 apt-get clean
